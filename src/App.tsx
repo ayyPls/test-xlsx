bda507a0336af6a93e01cccd61778d5dd3ebac5c
ayyPls <saispaev55@mail.ru>
2022-12-21 13:46:13 +0700

    clear empty columns

--- src/App.tsx
+++ src/App.tsx
@@ -60,6 +60,14 @@ function App() {
       return attribute ? attribute : `attribute_${attributeIndex}`
     }
 
+    const getVariableValue = (variable: string, variableIndex: number) => {
+      return variable ? variable : `variable_${variableIndex}`
+    }
+
+    const getSheetColumn = (listData: ISheetRow[], columnIndex: number) => {
+      return listData.map(row => row[columnIndex])
+    }
+
     const { files } = event.target;
     const fileReader = new FileReader();
     fileReader.onload = (ev) => {
@@ -68,21 +76,27 @@ function App() {
         // массив для списка листов файла
         const temp: Array<ISheetList> = []
         // итерация по листам файла
-
         Object.values(workbook.Sheets).map((sheet, index) => {
           // конвертируем в нужный формат
-          const sheetData = XLSX.utils.sheet_to_json(sheet, { header: 1, blankrows: false, defval: '' }) as ISheetRow[]
-          // первая строка экселя
-          const [variableSchemeArray, ...variablesData] = sheetData
+          const rawSheetData = XLSX.utils.sheet_to_json(sheet, { header: 1, blankrows: false, defval: '' }) as ISheetRow[]
+
+          // убираем пустые столбцы
+          const clearSheetData = rawSheetData.map((row, rowIndex) => {
+            return row.filter((cell, cellIndex) => getSheetColumn(rawSheetData, cellIndex).some(val => val !== ''))
+          })
+
+          // получаем схему для переменной из первой строки таблицы
+          const [variableSchemeArray] = clearSheetData
           // берем название переменной и ее атрибуты из первой строки
           const [variableName, ...variableAttributes] = variableSchemeArray
 
           // форматируем в удобный формат данные по переменной
-          const data: IVariable[] = sheetData.map(sheetRow => {
+          const data: IVariable[] = clearSheetData.map(sheetRow => {
+            const [variableValue, ...variableAttributeValues] = sheetRow
             return {
-              variableValue: sheetRow[0],
+              variableValue,
               attributes: Object.fromEntries(variableAttributes.map(
-                (attribute, attributeNumber) => [getAttributeName(attribute, attributeNumber), sheetRow[attributeNumber]]
+                (attribute, attributeNumber) => [getAttributeName(attribute, attributeNumber + 1), variableAttributeValues[attributeNumber]]
               ))
             }
           })
@@ -94,10 +108,10 @@ function App() {
             list: workbook.SheetNames[index],
           })
         })
-        console.log(temp)
         setSheetTable(temp)
 
       } catch (e) {
+        console.log(e)
         console.log('Неверный тип файла');
         return;
       }
@@ -116,17 +130,24 @@ function App() {
           ? <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <TableContainer component={Paper} sx={{ maxWidth: '80vw', maxHeight: '80vh' }}>
               <SheetListControl currentTab={currentTab} table={sheetTable} setCurrentTab={(val: number) => setCurrentTab(val)} />
-              <>
+              <div style={{ display: 'flex', alignItems: 'flex-start', flexDirection: 'column' }}>
                 <Typography fontSize="16px">{sheetTable[currentTab - 1].variableName}</Typography>
                 <Typography fontSize='14px' color="#1976D2">Переменная</Typography>
-              </>
+              </div>
               <Table stickyHeader>
-                {/* TODO: add head of table with variable attributes */}
                 <TableHead>
                   <TableRow>
                     <TableCell padding="checkbox">
                       <Checkbox />
                     </TableCell>
+                    <TableCell>
+                      Значения переменной
+                    </TableCell>
+                    {
+                      Object.keys(sheetTable[currentTab - 1].data[0].attributes).map(
+                        (attributeKey, index) => <TableCell key={index} sx={{ color: '#1976D2' }}>{attributeKey}</TableCell>
+                      )
+                    }
                   </TableRow>
                 </TableHead>
                 <TableBody>
@@ -158,7 +179,7 @@ function App() {
 
 const SheetListControl: FC<{ table: ISheetList[], currentTab: number, setCurrentTab: (newValue: number) => void }> = ({ table, currentTab, setCurrentTab }) => {
   return <Tabs value={currentTab} onChange={(_, value) => setCurrentTab(value)}>
-    {table.map((sheet, index) => <Tab value={index + 1} color={currentTab === index + 1 ? '#1976D2' : undefined} label={sheet.list} />)}
+    {table.map((sheet, index) => <Tab key={index} value={index + 1} color={currentTab === index + 1 ? '#1976D2' : undefined} label={sheet.list} />)}
   </Tabs>
 }
 
